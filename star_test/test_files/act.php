507d204db9a7b605893db10efd7480712060a1d5
takayuki watanabe <watanabe@mono-96.jp>
2018-04-26 13:47:28 +0900

    test up mae

--- star_test/test_files/act.php
+++
@@ -1,6 +0,0 @@
-(new Image).src="https://act-jp.rd.linksynergy.com/cc";
-(new Image).src="https://ads-jp.rd.linksynergy.com/cc";
-
-el = document.createElement("script");
-el.src = "//jp-tags.rd.linksynergy.com/js/1032?pt=prod&_="+(new Date().getTime());
-document.body.appendChild(el);
